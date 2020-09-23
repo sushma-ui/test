52b2f2f58b8de53eae04ed491ed37183cc01a30b
sushma-ui <71724182+sushma-ui@users.noreply.github.com>
2020-09-23 23:35:18 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+    System.out.println("Hello, World_java_program_testing_with_jenkins");
   }
 }
